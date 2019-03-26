4a860b517b9127b8e86faa69c3c1ec2c29ecc81b
Premysl Vyhnal <44365000+pvyhnal-generalbytes@users.noreply.github.com>
2019-03-26 14:05:56 +0100

    keep git history while renaming packages to dashj

--- migrate.sh
+++ migrate.sh
@@ -8,21 +8,30 @@ done
 
 set -x
 shopt -s globstar
-D=`date +%Y%m%d`
-CURVERSION=`grep '<artifactId>dashj-parent</artifactId>' -A1 pom.xml  | tail -1 | sed 's/.*<version>\(.*\)<\/version>.*/\1/'`
 
-sed -i '/<module>wallettemplate<\/module>/d' pom.xml
-sed -i "s/<version>$CURVERSION<\/version>/<version>$CURVERSION-$D<\/version>/" {,**/}pom.xml
-sed -i 's/org\.bitcoinj\./org\.dashj\./g' **/*.java **/*.proto
-sed -i 's/org\.bitcoin\([.;]\)/org\.dash\1/g' **/*.java
+
 rename -v 's/org\/bitcoinj/org\/dashj/' **/org/bitcoinj
 rename -v 's/org\/bitcoin/org\/dash/' **/org/bitcoin
 rename 's/org\.bitcoin\./org\.darkcoin\./' **
-
 git add .
 git commit -m "rename bitcoinj package to dashj"
 
-git cherry-pick 53827dbf2e8bdf0f5deb4d09ee1e9ec851a6eb10 # GB patch - dpub
+sed -i 's/org\.bitcoinj\./org\.dashj\./g' **/*.java **/*.proto
+sed -i 's/org\.bitcoin\([.;]\)/org\.dash\1/g' **/*.java
+git add .
+git commit -m "use bitcoinj package"
+
+D=`date +%Y%m%d`
+CURVERSION=`grep '<artifactId>dashj-parent</artifactId>' -A1 pom.xml  | tail -1 | sed 's/.*<version>\(.*\)<\/version>.*/\1/'`
+sed -i '/<module>wallettemplate<\/module>/d' pom.xml
+sed -i "s/<version>$CURVERSION<\/version>/<version>$CURVERSION-$D<\/version>/" {,**/}pom.xml
+git add .
+git commit -m "bump GB version"
+
+# GB patch - dpub
+git remote add gb git@github.com:GENERALBYTESCOM/dashj.git
+git fetch gb
+git cherry-pick 53827dbf2e8bdf0f5deb4d09ee1e9ec851a6eb10
 
 
 # mvn -Pno-build-bls -DskipTests clean install
